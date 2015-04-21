394943be5af9613cde7a3685a832315d0cc22d8d
dailiqi <richiedai@live.com>
2015-04-21 16:14:08 +0800

    by dai

---
+++ protected/config/offline.php
@@ -0,0 +1,40 @@
+<?php
+define('ROOT', dirname(dirname(__FILE__)));
+define('LOG_PATH', dirname(dirname(dirname(ROOT))));
+define('BASE_DIR', ROOT);
+define('IMG_PATH', dirname(ROOT) . '/webroot/');
+class Config_Fashion {
+    public static $config = array(
+        "defaultController" => "index",
+        "basePath" => BASE_DIR,
+        'import' => array(
+            'application.controllers.*',
+            'application.models.*',
+            'application.models.account.*',
+            'application.models.cloth.*',
+            'application.models.user.*',
+            'application.models.mix.*',
+
+            'application.models.dao.*',
+            'application.components.*',
+            'application.extensions.*',
+        ),
+        'components' => array(
+            'smarty' => array(
+                'class' => 'application.extensions.CSmarty',
+            ),
+            'db' => array(
+                'connectionString' => 'mysql:host=localhost;port=3306;dbname=fashion',
+                'username' => 'root',
+                'password' => '',
+            ),
+            'urlManager'=>array(
+                'urlFormat'=>'path',
+                'showScriptName'=>true,
+                'rules'=>array(
+                    '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+                ),
+            ),
+        ),
+    );
+}
--- webroot/index.php
+++ webroot/index.php
@@ -5,10 +5,12 @@ error_reporting(E_ALL);
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-require_once(dirname(dirname(__FILE__)).'/protected/config/conf.php');
+//require_once(dirname(dirname(__FILE__)).'/protected/config/conf.php');
+require_once(dirname(dirname(__FILE__)).'/protected/config/offline.php');
 $config = Config_Fashion::$config;
 require_once(dirname(dirname(__FILE__)) . '/yii/framework/yii.php');
 // create a Web application instance and run
-require_once(dirname(dirname(__FILE__)) . '/protected/base/WebApp.php');
+//require_once(dirname(dirname(__FILE__)) . '/protected/base/WebApp.php');
 //Yii::createWebApplication($config)->run();
-Yii::createApplication('WebApp', $config)->run();
+//Yii::createApplication('WebApp', $config)->run();
+Yii::createWebApplication($config)->run();
